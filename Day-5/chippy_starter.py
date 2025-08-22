240b61923b7df151d0e2670c0076a55ce62e7f22
sunilm33 <78884617+sunilm33@users.noreply.github.com>
2025-08-22 08:52:07 -0700

    Create chippy_starter.py

---
+++ Day-5/chippy_starter.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello I am Chippie!")
+    name = input("What is your name?")
+    print(f"Hello {name}! Nice to meet you")
+    print("We will be blasting off today, but our ship isnt prepped.")
+    print("First we need to figure out how many people are coming.")
+
+main()
