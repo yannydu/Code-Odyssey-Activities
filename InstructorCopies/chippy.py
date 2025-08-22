0365ad3cba2e62590e9fa6c7204610414201f79c
sunilm33 <78884617+sunilm33@users.noreply.github.com>
2025-08-22 10:19:26 -0700

    Added distances for travel

--- InstructorCopies/chippy.py
+++ InstructorCopies/chippy.py
@@ -4,6 +4,8 @@ def main():
     print(f"Hello {name}! Nice to meet you")
     print("We will be blasting off today, but our ship isnt prepped.")
     print("First we need to figure out how many people are coming.")
+
+    # Crew 
     crew_size = int(input("How many people do you want on the ship? "))
     print("Please list names of the crew:")
     crew = [name]
@@ -16,13 +18,39 @@ def main():
             print(f"{c_name}")
         else:
             print(f"{c_name}, ", end=" ")
-    print("Now we need to know how long we are going for to decide fuel and food reserves.")
+
+    
+    # Propulsion
+    print("Now that we have the fuel, we need to know the distance for the trip")
+    print("Please list all the planets you want to travel to and the distances")
+    planet_amounts = int(input("How many planets are we visiting? "))
+    old_planet = "Earth"
+    planets = ["Earth"]
+    distances = []
+    for i in range(planet_amounts):
+        planet = input("Planet Name: ")
+        distance = int(input(f"Distance (in kilometers) to travel from {old_planet} to {planet}: "))
+        old_planet = planet
+        planets.append(planet)
+        distances.append(distance)
+    print(f"We will be travelling to ", end=" ")
+    for p in planets:
+        if p == planets[-1]: 
+            print(f"{p}")
+        else:
+            print(f"{p}, ", end=" ")
+    total_distance = 0
+    for dis_size in distances:
+        total_distance += dis_size
+    print(f"We will be travelling {total_distance}km.")
+    
+    # Food
+    print("Now we need to know how long we are going to get our food reserves.")
     days = int(input("How many days are you going? "))
-    # Fuel needs 100L per day gone
-    litres_needed = 100
-    fuel = days * litres_needed
     # Crew needs 3 bars per person per day
     bars_needed = 3
     food = bars_needed * crew_size * days
-    print(f"We need to bring {fuel}L of fuel and {food} nutriyum bars.")
+    print(f"We need to bring {food} nutriyum bars.")
+
+
 main()
